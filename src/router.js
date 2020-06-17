2135052182f4326244054fd247fb7f9a1083b8e0
sumi <31235393+monstersumi@users.noreply.github.com>
2020-06-17 16:17:34 +0800

    补充jsDoc注释

--- src/router.js
+++ src/router.js
@@ -15,56 +15,56 @@ Vue.use(Router);
 
 export default new Router({
 	routes: [
-		{
+		{//主路由
 			path: '',
 			name: 'home',
 			component: Home,
 			redirect: '/index',
-			children: [
-				{
+			children: [//产品相关
+				{//首页
 					path: 'index',
 					name: 'index',
 					component: Index
 				},
-				{
+				{//产品主页
 					path: 'product/:id',
 					name: 'product',
 					component: Product
 				},
-				{
+				{//产品详情
 					path: 'detail/:id',
 					name: 'detail',
 					component: Detail
 				}
 			]
 		},
-		{
+		{//购物车
 			path: '/cart',
 			name: 'cart',
 			component: Cart
 		},
-		{
+		{//订单相关
 			path: '/order',
 			name: 'order',
 			component: Order,
 			redirect: 'order/list',
 			children: [
-				{
+				{//订单列表
 					path: 'list',
 					name: 'order-list',
 					component: OrderList
 				},
-				{
+				{//订单确认
 					path: 'confirm',
 					name: 'order-confirm',
 					component: OrderConfirm
 				},
-				{
+				{//订单支付
 					path: 'pay',
 					name: 'order-pay',
 					component: OrderPay
 				},
-				{
+				{//支付宝中转
 					path: 'alipay',
 					name: 'alipay',
 					component: AliPay
--- src/storage/index.js
+++ src/storage/index.js
@@ -1,10 +1,25 @@
+//声明所用的sessionStorage的库名
 const STORAGE_KEY = 'mall';
 
+/**
+ * 封装Eslint可用的hasOwnProperty方法
+ * @param {Object} obj - 需要查询的对象
+ * @param {string} key - 需要查询的key
+ * @returns {boolean} - 返回是否含有此键
+ */
 function hasOwnProperty(obj, key) {
 	return Object.prototype.hasOwnProperty.call(obj, key);
 }
 
 export default {
+	/**
+	 * 设置sessionStorage的方法.
+	 * @param {string} key - sessionStorage的key
+	 * @param {string/number/Object} value - sessionStorage的key对应的值
+	 * @param {$Rest} module_name - 如果有更多层key嵌套，按照嵌套顺序传入key值
+	 * @example
+	 * storage.setItem(key,value,module_name1,module_name2,module_name3,module_name4...)
+	 */
 	setItem(key, value, ...module_name) {
 		if (value) {
 			if (module_name.length) {
@@ -22,6 +37,14 @@ export default {
 		}
 
 	},
+	/**
+	 * 获取sessionStorage的方法
+	 * @param {string} key - sessionStorage的key
+	 * @param {$Rest} module_name - 如果有更多层key嵌套，按照嵌套顺序传入key值
+	 * @returns {*|{}} - 获取到的sessionStorage值或空{}
+	 * @example
+	 * storage.getItem(key,module_name1,module_name2,module_name3,module_name4...)
+	 */
 	getItem(key, ...module_name) {
 		if (module_name.length) {
 			const _value = this.getItem(module_name.shift(0), ...module_name);
@@ -31,9 +54,20 @@ export default {
 		}
 		return this.getStorage()[key] || {};
 	},
+	/**
+	 *
+	 * @returns {JSON} - 返回当前窗口的sessionStorage
+	 */
 	getStorage() {
 		return JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) || '{}');
 	},
+	/**
+	 * 清除sessionStorage的方法
+	 * @param {string} key - 需要清除值的sessionStorage的key
+	 * @param {$Rest} moudle_name - 如果有更多层key嵌套，按照嵌套顺序传入key值
+	 * @example
+	 * storage.clear(key,module_name1,module_name2,module_name3,module_name4...)
+	 */
 	clear(key, ...moudle_name) {
 		const _storage = this.getStorage();
 		if (key) {
